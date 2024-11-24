e8b719225e80429927884e665ee6d942dd513c07
Nils.Fitjar <nfi005@uib.no>
2024-11-24 11:28:35 +0100

    docs: added comments
    Corrected what element the event is being set on, from document to form,
    and corrected what element the greeting is being set on, from form to
    greet-msg p-tag.

--- src/Main.purs
+++ src/Main.purs
@@ -10,42 +10,62 @@ import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Promise as P
 import Tauri (greet)
-import Web.DOM.Element (toNode, Element)
-import Web.DOM.Node (Node, setTextContent)
+import Web.DOM.Element (Element, toEventTarget, toNode)
+import Web.DOM.Node (setTextContent)
 import Web.DOM.ParentNode (QuerySelector(..), querySelector)
 import Web.Event.Event (Event, EventType(..), preventDefault)
 import Web.Event.EventTarget (addEventListener, eventListener)
 import Web.HTML (window)
-import Web.HTML.HTMLDocument (toEventTarget, toParentNode)
-import Web.HTML.Window (document)
+import Web.HTML.HTMLDocument (toParentNode)
 import Web.HTML.HTMLInputElement as I
-
+import Web.HTML.Window (document)
+-- This example is the same as the Tauri + JavaScript example
 main :: Effect Unit
-main = do
-  w <- window
-  doc <- document w
+main = do 
+  w <- window -- Gives the current window
+  doc <- document w -- Gives the document to the window
+  {- Because of PureScript, we need to turn the document, which is of type
+  -- HTMLDocument, into a ParentNode, so that we can use querySelection to get
+  -- the form and input.
+  -}
   let documentParent = toParentNode doc
+  {- Equivalent to:
+  -- greetInputEl = document.querySelector("#greet-input");
+  -- Except querySelector returns Maybe Element, so for the greetInputEl, we
+  -- need to "cast" it to HTMLInputElement, but our Element might not be of
+  -- that type, so we need to handle that aswell.
+  -}
   greetFormElementM <- querySelector (QuerySelector "#greet-form") documentParent
   greetInputElementM <- querySelector(QuerySelector "#greet-input") documentParent
-  case (Tuple greetFormElementM (getInputElement greetInputElementM)) of
-    Tuple (Just greetFormElement) (Just inputElement) -> do
-      let greetFormNode = toNode greetFormElement
-      let eventTarget = toEventTarget doc
-      greetEvent <- eventListener (greetEffect greetFormNode)
+  greetMsgElementM <- querySelector(QuerySelector "#greet-msg") documentParent
+  -- This is not great PureScript code
+  case (Tuple greetFormElementM (Tuple (greetInputElementM >>= I.fromElement) greetMsgElementM)) of
+    Tuple (Just greetFormElement) (Tuple (Just inputElement) (Just greetElement)) -> do
+      -- To add an eventListener, we need a Node type, which all Elements are.
+      let eventTarget = toEventTarget greetFormElement
+      greetEvent <- eventListener (greetCallback greetElement)
       addEventListener (EventType "submit") greetEvent false eventTarget
       where
-        greetEffect :: Node -> Event -> Effect Unit
-        greetEffect greetFormNode evt = do
+        greetCallback :: Element -> Event -> Effect Unit
+        greetCallback greetMsgElement evt = do
+          -- Preventing the form from being sent
           _ <- preventDefault evt
+          {- Getting the value from the inputElement, we have to do this in the
+          -- callback, because we are only interested in the contents of the
+          -- input-element *after* the user has clicked on submit. If we did
+          -- this before, i.e. in the main-function, it would be empty.
+          -}
           name <- I.value inputElement
-          _ <- P.then_ (setGreeting greetFormNode) (greet name)
+          -- Invoking greet
+          let promise = (greet name)
+          {- Using invoke makes things a little more complicated, because it is
+          -- a promise, so we need *another* callback.
+          -}
+          _ <- P.then_ (setGreetingCallback greetMsgElement) promise
           mempty
-        setGreeting :: Node -> String -> Effect (P.Promise Unit)
-        setGreeting greetFormNode greeting = do
-          _ <- setTextContent greeting greetFormNode
+        setGreetingCallback :: Element -> String -> Effect (P.Promise Unit)
+        setGreetingCallback greetMsgElement greeting = do
+          -- Finally, we can set the content of the greetMsg element.
+          _ <- setTextContent greeting (toNode greetMsgElement)
           pure (P.resolve unit)
     _ -> mempty
-
-getInputElement :: Maybe Element -> Maybe I.HTMLInputElement
-getInputElement (Just no) = I.fromElement no
-getInputElement _ = Nothing
